00636f6f6a807be5f8a17427a842ef05bbfffed0
Chiliano Arcarese <19277@studenti.marconiverona.edu.it>
2023-07-06 18:16:01 +0000

    commit for merging

--- plotly-app/app.py
+++ plotly-app/app.py
@@ -1,10 +1,51 @@
 import os
 from dash import Dash, html, dcc
 
-
 import dash_bootstrap_components as dbc
 import pandas as pd
 import dash
+from keras import Sequential
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class KerasWrapper(BaseEstimator, TransformerMixin):
+    def __init__(self, num_epochs=100, verbose=0, **kwargs):
+        self.num_epochs = num_epochs
+        self.verbose = verbose
+        self.model = Sequential(**kwargs)
+        
+    def fit(self, X, y):
+        self.model.compile(loss='mean_absolute_error', optimizer='adam', metrics=['mean_squared_error'])
+        self.model.fit(X, y, validation_split = 0.2 ,epochs=self.num_epochs, verbose=self.verbose)
+        return self
+    
+    def transform(self, X):
+        # Perform any necessary transformations
+        return X
+    
+    def predict(self, X):
+        return self.model.predict(X)
+    
+    
+    def __getstate__(self):
+        # Get the state to be pickled
+        state = self.__dict__.copy()
+
+        # Remove the unpicklable model object
+        del state['model']
+
+        return state
+
+    def __setstate__(self, state):
+        # Restore the state from the unpickled state
+        self.__dict__.update(state)
+
+        if 'model_weights' in state:
+            self.model = Sequential()
+            self.model.set_weights(state['model_weights'])
+            self.model.compile(loss=state['model_loss'], optimizer=state['model_optimizer'], metrics=state['model_metrics'])
+        else:
+            self.model = None
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
@@ -30,44 +71,34 @@ sidebar = html.Div(
             [
                 dbc.NavLink(
                     [
-                        html.I(className="fa-solid fa-user fa-2x"),
-                        html.Br(),
-                        html.Span("User"),
-                    ],
-                    href="/user",
-                    active="exact",
-                    style={"text-align": "center"},
-                ),
-                html.Hr(className="sidebar-hr"),
-                dbc.NavLink(
-                    [
                         html.I(className="fa-solid fa-square-poll-vertical fa-2x"),
-                        html.Br(),
-                        html.Span("Raw FBK Data"),
+                        html.Br(), 
+                        html.Span("Raw FBK Data")
                     ],
                     href="/",
                     active="exact",
-                    style={"text-align": "center"},
+                    style={"text-align": "center"}
+                    
                 ),
                 dbc.NavLink(
                     [
                         html.I(className="fa-solid fa-chart-line fa-2x"),
-                        html.Br(),
+                        html.Br(), 
                         html.Span("Fitted FBK Data"),
                     ],
                     href="/fbk",
                     active="exact",
-                    style={"text-align": "center"},
+                    style={"text-align": "center"}
                 ),
                 dbc.NavLink(
                     [
                         html.I(className="fa-solid fa-smog fa-2x"),
-                        html.Br(),
+                        html.Br(), 
                         html.Span("APPA Data"),
                     ],
                     href="/appa",
                     active="exact",
-                    style={"text-align": "center"},
+                    style={"text-align": "center"}
                 ),
             ],
             vertical=True,
@@ -91,4 +122,5 @@ if __name__ == "__main__":
     if os.getenv("DEBUG"):
         app.run(debug=True)
     else:
-        app.run_server(port=8051, host="0.0.0.0", debug=True)
+        # Production
+        app.run_server(port=8051, host="0.0.0.0", debug=False)
--- plotly-app/pages/fbk.py
+++ plotly-app/pages/fbk.py
@@ -1,15 +1,103 @@
+from statistics import mean
 from dash import html, dcc, Input, Output, callback
-from .utils import utils
-
+from .utils import utils, querys
+from db_utils import load_data_from_psql
 import dash_bootstrap_components as dbc
 import plotly.graph_objs as go
 import dash_daq as daq
 import pandas as pd
 import numpy as np
 import dash
-import logging
+import joblib
+from tensorflow.keras.models import load_model
+from datetime import timedelta, datetime
 
 dash.register_page(__name__)
+
+
+
+def get_data_day(start, end) -> pd.DataFrame:
+    print("NOT CACHED DAY")
+    fbk_data=  load_data_from_psql(testnewdf(start, end))
+    #print(fbk_data)
+    return (fbk_data)
+
+def testnewdf(start, end):
+    
+    return f"""WITH sensor_data AS (
+    SELECT
+        n.description AS node_description,
+        s.name AS sensor_description,
+        p.sensor_ts AS ts,
+        pd.r1 AS heater_res,
+        pd.r2 AS signal_res,
+        pd.volt AS volt,
+        p.attrs::json->>'P' AS p,
+        p.attrs::json->>'T' AS t,
+        p.attrs::json->>'RH' AS rh
+    FROM packet_data pd
+        LEFT JOIN packet p ON p.id = pd.packet_id
+        LEFT JOIN sensor s ON s.id = pd.sensor_id
+        LEFT JOIN node n ON n.id = p.node_id
+    where p.sensor_ts BETWEEN '{start}' AND '{end}' and n.id = 1
+)
+SELECT
+    ts,
+    MAX(p) AS p,
+    MAX(t) AS t,
+    MAX(rh) AS rh,
+    MAX(CASE WHEN sensor_description = 'S1_ID' THEN heater_res END) AS S1_R1,
+    MAX(CASE WHEN sensor_description = 'S1_ID' THEN signal_res END) AS S1_R2,
+    MAX(CASE WHEN sensor_description = 'S1_ID' THEN volt END) AS S1_Voltage,
+    MAX(CASE WHEN sensor_description = 'S2_ID' THEN heater_res END) AS S2_R1,
+    MAX(CASE WHEN sensor_description = 'S2_ID' THEN signal_res END) AS S2_R2,
+    MAX(CASE WHEN sensor_description = 'S2_ID' THEN volt END) AS S2_Voltage,
+    MAX(CASE WHEN sensor_description = 'S3_ID' THEN heater_res END) AS S3_R1,
+    MAX(CASE WHEN sensor_description = 'S3_ID' THEN signal_res END) AS S3_R2,
+    MAX(CASE WHEN sensor_description = 'S3_ID' THEN volt END) AS S3_Voltage,
+    MAX(CASE WHEN sensor_description = 'S4_ID' THEN heater_res END) AS S4_R1,
+    MAX(CASE WHEN sensor_description = 'S4_ID' THEN signal_res END) AS S4_R2,
+    MAX(CASE WHEN sensor_description = 'S4_ID' THEN volt END) AS S4_Voltage,
+    MAX(CASE WHEN sensor_description = 'S5_ID' THEN heater_res END) AS S5_R1,
+    MAX(CASE WHEN sensor_description = 'S5_ID' THEN signal_res END) AS S5_R2,
+    MAX(CASE WHEN sensor_description = 'S5_ID' THEN volt END) AS S5_Voltage,
+    MAX(CASE WHEN sensor_description = 'S6_ID' THEN heater_res END) AS S6_R1,
+    MAX(CASE WHEN sensor_description = 'S6_ID' THEN signal_res END) AS S6_R2,
+    MAX(CASE WHEN sensor_description = 'S6_ID' THEN volt END) AS S6_Voltage,
+    MAX(CASE WHEN sensor_description = 'S7_ID' THEN heater_res END) AS S7_R1,
+    MAX(CASE WHEN sensor_description = 'S7_ID' THEN signal_res END) AS S7_R2,
+    MAX(CASE WHEN sensor_description = 'S7_ID' THEN volt END) AS S7_Voltage,
+    MAX(CASE WHEN sensor_description = 'S8_ID' THEN heater_res END) AS S8_R1,
+    MAX(CASE WHEN sensor_description = 'S8_ID' THEN signal_res END) AS S8_R2,
+    MAX(CASE WHEN sensor_description = 'S8_ID' THEN volt END) AS S8_Voltage
+FROM sensor_data
+GROUP BY ts
+ORDER BY ts;"""
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"  
+
+
+
+
+
+pipeline2 = joblib.load('data/pipeline.pkl')
+
+# Load the Keras model separately
+
+model = load_model('data/model.h5')
+#predictions = inference_func(input_data)['output'].numpy()
+
+# Assign the loaded model to the pipeline
+pipeline2.steps[-1][1].model = model
+
+print(pipeline2.steps[-1][1].model)
+
+pipeline2.steps[-1][1].model.compile(loss='mean_absolute_error', optimizer='adam', metrics=['mean_squared_error'])
+
+
+
+
 
 
 df = utils.get_prediction_data()
@@ -26,15 +114,17 @@ df["O3_pred"] += np.random.rand(len(df)) * 10 - 5
 
 fbk_stations = ["Parco S. Chiara", "Via Bolzano"]
 pollutants = [
-    dict(label="Nitrogen Dioxide", value="NO2"),
     dict(label="Ozone", value="O3"),
-    dict(label="Carbon Monoxide", value="CO"),
+    dict(label="Nitrogen Dioxide", value="NO2"),
+    dict(label="PM10", value="PM10"),
+    dict(label="Sulfur Dioxide", value="SO2"),
 ]
 
-title = html.Div(
-    "Fitted FBK Data",
-    className="header-title",
-    style={"text-align": "center", "margin-bottom": "0.25rem"},
+title = html.Div("Fitted FBK Data", className="header-title")
+
+# build dropdown of stations
+dropdown = dcc.Dropdown(
+    fbk_stations, id="selected-station", className="dropdown", value=fbk_stations[0]
 )
 
 download_btn = dbc.Button(
@@ -65,37 +155,28 @@ gas_btns = html.Div(
         label_class_name="btn btn-outline-primary",
         label_checked_class_name="active",
         options=pollutants,
-        value="NO2",
+        value="O3",
     ),
     className="radio-group",
 )
 
-# build dropdown of stations
-dropdown = dcc.Dropdown(
-    fbk_stations, id="selected-station", className="dropdown", value=fbk_stations[0]
-)
-dropdown_wrapper = html.Div(
-    [
-        dropdown,
-    ],
-    className="dropdownWrapper",
-)
-
 header = html.Div(
-    [title, dropdown_wrapper, gas_btns, download_btn, download_it],
-    className="section-header",
+    [title, dropdown, download_btn, download_it, gas_btns], className="section-header"
 )
 
 graph_selectors = html.Div(
     [
         html.Div(
-            dcc.Dropdown(
-                id="selected-period",
-                options=["last 24h", "last week", "last month", "last year", "all"],
-                value="last week",
-                className="dropdown",
-            ),
-            className="dropdownWrapper",
+            [
+                "Display: ",
+                dcc.Dropdown(
+                    id="selected-period",
+                    options=["March","April", "May", "June", "July"],
+                    className="dropdown",
+                    value="April",
+                ),
+            ],
+            className="graph-dropdown",
         ),
         daq.ToggleSwitch(
             id="toggle-comparison",
@@ -105,7 +186,7 @@ graph_selectors = html.Div(
             value=True,
         ),
     ],
-    # className="d-flex justify-content-between",
+    className="d-flex flex-grow justify-content-between",
 )
 
 comparison_graph = html.Div(
@@ -118,8 +199,12 @@ comparison_graph = html.Div(
                 "displaylogo": False,
             },
         ),
-    ],
-    className="pretty_container",
+        graph_selectors,
+        dcc.Graph(
+            id="test-graph",
+            style=dict(height="50vh"),
+        ),
+    ]
 )
 
 
@@ -149,21 +234,54 @@ def update_comparison_graph(
     Returns:
         plotly.graph_objs.Figure: the graph
     """
-    pollutant_real = selected_pollutant + "_real"
-    pollutant_pred = selected_pollutant + "_pred"
 
+    
+    months= {
+    'January': ('2023-01-01', '2023-01-31'),
+    'February': ('2023-02-01', '2023-02-28'),
+    'March': ('2023-03-01', '2023-03-31'),
+    'April': ('2023-04-01', '2023-04-30'),
+    'May': ('2023-05-01', '2023-05-31'),
+    'June': ('2023-06-01', '2023-06-30'),
+    'July': ('2023-07-01', '2023-07-31'),
+    'August': ('2023-08-01', '2023-08-31'),
+    'September': ('2023-09-01', '2023-09-30'),
+    'October': ('2023-10-01', '2023-10-31'),
+    'November': ('2023-11-01', '2023-11-30'),
+    'December': ('2023-12-01', '2023-12-31')
+    }
+    start = months[selected_period][0]
+    end = months[selected_period][1]
+    
+    raw_data = get_data_day(start, end)
+    
+    utc_now = datetime.utcnow()
+    asd = datetime.now() - utc_now
+    raw_data['ts'] = raw_data['ts'] + asd
+    test = raw_data.drop('ts',axis=1)
+    
+    y_pred=pipeline2.predict(test.values)
+
+    appa_data = load_data_from_psql(querys.q_custom_appa(start=start, end=end))
+    
     fig = go.Figure()
+    
+    new_df = pd.DataFrame()
+    new_df['ts'] = raw_data['ts'].copy()
 
-    data = get_mean(df, selected_station, selected_pollutant, selected_period)
-
-    # data = df[df.Station == selected_station]
-
-    data = data[["Time", pollutant_real, pollutant_pred]]
-
-    # prediction graph
+    new_df['O3'] = y_pred[:,3].tolist()
+    new_df['PM10'] = y_pred[:,0].tolist()
+    new_df['NO2'] = y_pred[:,1].tolist()
+    new_df['SO2'] = y_pred[:,2].tolist()
+    
+    
+    new_df= new_df.set_index('ts').resample('1H').mean().reset_index()
+    data = appa_data[(appa_data['stazione'] == 'Parco S. Chiara') & (appa_data['inquinante'] == selected_pollutant) & (['avg'])] 
+    df = new_df.merge(data[['min','avg']], left_on='ts', right_on='min', how="left").drop('min',axis=1)
+    
     fig.add_trace(
         go.Scatter(
-            x=data["Time"], y=data[pollutant_pred], mode="lines+markers", name="FBK"
+            x=df["ts"], y=df[selected_pollutant], mode="lines+markers", name="FBK"
         )
     )
 
@@ -173,8 +291,8 @@ def update_comparison_graph(
         # appa data graph
         fig.add_trace(
             go.Scatter(
-                x=data["Time"],
-                y=data[pollutant_real],
+                x=df["ts"],
+                y=df['avg'],
                 mode="lines+markers",
                 name="APPA",
             )
@@ -185,24 +303,15 @@ def update_comparison_graph(
     fig.update_layout(
         margin=dict(l=5, r=5, t=20, b=0),
         plot_bgcolor="white",
-        paper_bgcolor="rgba(0,0,0,0)",
         title=dict(
             x=0.5,
             text=title,
+            font_family="Sans serif",
             xanchor="center",
             yanchor="top",
         ),
-        legend={
-            "x": 0,
-            "y": 1,
-            "yanchor": "bottom",
-            "xanchor": "left",
-            "orientation": "h",
-            "bgcolor": "rgba(0,0,0,0)",
-        },
     )
     fig.update_yaxes(title_text="Value", fixedrange=True)
-    fig.update_layout(modebar=dict(bgcolor="#ffffff"))
 
     return fig
 
@@ -264,15 +373,6 @@ def get_mean(
 
 
 layout = html.Div(
-    [
-        header,
-        html.Div(
-            [
-                comparison_graph,
-                graph_selectors,
-            ],
-            className="fbk-main-plot",
-        ),
-    ],
+    [header, html.Div([comparison_graph], className="fbk-main-plot")],
     className="section fullHeight",
 )
